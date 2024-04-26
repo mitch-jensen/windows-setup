2d499efe0869124b6d61bc85195f050872bac8c8
mitch-jensen <65543196+mitch-jensen@users.noreply.github.com>
2024-04-26 20:44:07 +1000

    Create winget-installers.ps1

---
+++ winget-installers.ps1
@@ -0,0 +1,11 @@
+winget install --id Git.Git -e --source winget
+winget install --id GitHub.cli -e --source winget
+winget install --id Microsoft.Powershell -e --source winget
+winget install --id Microsoft.VisualStudioCode -e --source winget
+winget install --id Microsoft.WindowsTerminal  -e --source winget 
+winget install --id Python.Launcher -e --source winget # allows you to run Python and specify a version (e.g., py -3.9 script.py or py -3.11 script.py)
+winget install --id Python.Python.3.9 -e --source winget
+winget install --id Python.Python.3.11 -e --source winget
+winget install --id Microsoft.DotNet.SDK.8 -e --source winget
+winget install --id Starship.Starship -e --source winget
+winget install --id JesseDuffield.lazygit -e --source winget
